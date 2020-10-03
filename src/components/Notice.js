d1fecc3e2e282bc3fb4a55b6d5bc6814608f8301
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-10-03 20:50:48 +0900

    [proceeding] pagination

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -1,16 +1,34 @@
 import React from "react";
+import ReactPaginate from "react-paginate";
 import "./Notice.css";
 
 function Notice({data}) {
+    const titleStyle = {
+        color: data.color
+    }
     const headStyle = {
         color: "white",
-        backgroundColor: data.color
+        backgroundColor: data.color,
     }
-    console.log(data)
+    const pageProps = {
+        pageCount: 100,
+        pageRangeDisplayed: 3,
+        marginPagesDisplayed: 1,
+        previousLabel: "\u003C",
+        nextLabel: "\u003E"
+    }
     return <div className="notice">
-        <div className="notice-title" style={{color: data.color}}>{data.title}</div>
+        <div
+            className="notice-title"
+            style={titleStyle}
+        >
+            {data.title}
+        </div>
         <div className="boards">
-            <div className="board" style={headStyle}>
+            <div
+                className="board"
+                style={headStyle}
+            >
                 <div className="id">순번</div>
                 <div className="title">제목</div>
                 <div className="views">조회수</div>
@@ -30,16 +48,15 @@ function Notice({data}) {
                 <div className="title">테스트 공지사항입니다.</div>
                 <div className="views">00</div>
             </div>
-            <div className="board">
-                <div className="id">00</div>
-                <div className="title">테스트 공지사항입니다.</div>
-                <div className="views">00</div>
-            </div>
-            <div className="board">
-                <div className="id">00</div>
-                <div className="title">테스트 공지사항입니다.</div>
-                <div className="views">00</div>
-            </div>
+        </div>
+        <div className="pagination" style={headStyle}>
+            <ReactPaginate
+                pageCount={pageProps.pageCount}
+                pageRangeDisplayed={pageProps.pageRangeDisplayed}
+                marginPagesDisplayed={pageProps.marginPagesDisplayed}
+                previousLabel={pageProps.previousLabel}
+                nextLabel={pageProps.nextLabel}
+            />
         </div>
     </div>
 }

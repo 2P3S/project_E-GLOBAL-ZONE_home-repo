46dc1ebb8c935917010f9a7f09cd69873c33f226
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-10-04 20:52:59 +0900

    [build] ver 1.0.0

---
+++ build/precache-manifest.bc2a67feb2fc83cb03d7b113c13d1082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd09193701c754119f71a460aa9bb32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec62fee47ed7ff22924",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "000bb3f4a4e93052b0cb",
+    "url": "/static/css/main.1ebc04a8.chunk.css"
+  },
+  {
+    "revision": "9ec62fee47ed7ff22924",
+    "url": "/static/js/2.680aaee4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.680aaee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000bb3f4a4e93052b0cb",
+    "url": "/static/js/main.0befe491.chunk.js"
+  },
+  {
+    "revision": "0ec82569c5b4ea17a123",
+    "url": "/static/js/runtime-main.13d4bfde.js"
+  }
+]);
